934f2afeddf61665e2a7ce87c990c5f90680e862
Radzev <kor.gregory@yahoo.com>
2024-09-25 14:20:01 +0500

    Create Password generator.py

---
+++ Password generator.py
@@ -0,0 +1,14 @@
+import string
+import random
+import re
+import hashlib
+import base64
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+password_length = int(input("Введите желаемую длину пароля: "))
+generated_password = generate_password(password_length)
+print("Ваш сгенерированный пароль:", generated_password)
